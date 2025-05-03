06e8691500aabe88b69e0cab6eb64c8dd44f0301
PavelBzrkv <pavel95959@gmail.com>
2025-05-03 21:49:45 +0500

    Task2

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+
+        whatNumber();
+    }
+private static void whatNumber() {
+    System.out.println("Введите число:");
+    int num = new Scanner(System.in).nextInt();
+    System.out.println(num % 2 == 0);
     }
 }
+
